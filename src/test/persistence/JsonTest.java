e362435b5771ab31f03338e98a0a7bd63588167d
Stephanie Xue <stephxue@students.cs.ubc.ca>
2025-02-20 15:22:05 -0800

    add helper tests in JsonTest

---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,42 @@
+package persistence;
+
+import model.ClinicalNote;
+import model.Date;
+import model.Patient;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+    protected void checkPatientGeneral(String firstName, String lastName, Date dob, int age, long phn, Patient pt) {
+        assertEquals(firstName, pt.getFirstName());
+        assertEquals(lastName, pt.getLastName());
+        assertEquals(dob, pt.getDateOfBirth());
+        assertEquals(age, pt.getAge());
+        assertEquals(phn, pt.getPersonalHealthNumber());
+    }
+
+    protected void checkPatientAllergies(List<String> allergies, Patient pt) {
+        assertEquals(allergies, pt.getAllergies());
+    }
+
+    protected void checkPatientMedications(List<String> medications, Patient pt) {
+        assertEquals(medications, pt.getMedications());
+    }
+
+    protected void checkPatientMedicalConditions(List<String> medicalConditions, Patient pt) {
+        assertEquals(medicalConditions, pt.getMedicalConditions());
+    }
+
+    protected void checkPatientClinicalNote(List<ClinicalNote> notes, Patient pt) {
+        assertEquals(notes, pt.getClinicalNotes());
+    }
+
+    protected void checkClinicalNote(String title, String body, String provider, Date visitDate, ClinicalNote note) {
+        assertEquals(title, note.getClinicalNoteTitle());
+        assertEquals(body, note.getClinicalNoteBody());
+        assertEquals(provider, note.getClinicalNoteProvider());
+        assertEquals(visitDate, note.getClinicalNoteDate());
+    }
+}
---
+++ src/test/persistence/JsonWriter.java
@@ -0,0 +1,15 @@
+package persistence;
+
+import model.Clinic;
+import model.ClinicalNote;
+import model.Date;
+import model.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonWriter {
+    
+}
