d3d95f23d90a67c74c5eef98a3fe8505bc844dc3
Stephanie Xue <stephxue@students.cs.ubc.ca>
2025-02-28 12:03:37 -0800

    add helper functions to JsonReaderTest and JsonWriterTest

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -42,78 +42,111 @@ public class JsonReaderTest extends JsonTest {
         JsonReader reader = new JsonReader("./data/testReaderGeneralClinic.json");
         try {
             Clinic clinic = reader.read();
-            assertEquals("My Clinic", clinic.getClinicName());
             List<Patient> patients = clinic.getPatients();
-            assertEquals(2, patients.size());
-
-            Date dateOfBirth1 = new Date(8, 16, 1965);
-            List<String> allergies1 = new ArrayList<>();
-            allergies1.add("penicillin");
-            allergies1.add("codeine");
-            List<String> medications1 = new ArrayList<>();
-            medications1.add("metformin");
-            medications1.add("ramipril");
-            medications1.add("amlodipine");
-            List<String> medicalConditions1 = new ArrayList<>();
-            medicalConditions1.add("diabetes");
-            medicalConditions1.add("hypertension");
-            String title1 = "Coughing";
-            String body1 = "Patient presenting with severe cough";
-            String provider1 = "Dr. Campbell";
-            Date visitDate1 = new Date(1, 16, 2025);
-            ClinicalNote note1 = new ClinicalNote(title1, body1, provider1, visitDate1);
-            String title2 = "Ear infection";
-            String body2 = "Patient presenting with infected ear";
-            String provider2 = "Dr. Singh";
-            Date visitDate2 = new Date(2, 20, 2025);
-            ClinicalNote note2 = new ClinicalNote(title2, body2, provider2, visitDate2);
-            List<ClinicalNote> notesList1 = new ArrayList<>();
-            notesList1.add(note1);
-            notesList1.add(note2);
-
-            checkPatientGeneral("Mya", "Cornell", dateOfBirth1, 59, 925245285, patients.get(0));
-            checkPatientAllergies(allergies1, patients.get(0));
-            checkPatientMedications(medications1, patients.get(0));
-            checkPatientMedicalConditions(medicalConditions1, patients.get(0));
-            checkPatientClinicalNotes(notesList1, patients.get(0));
-            checkClinicalNote(title1, body1, provider1, visitDate1, note1);
-            checkClinicalNote(title2, body2, provider2, visitDate2, note2);
-
-            Date dateOfBirth2 = new Date(11, 8, 1971);
-            List<String> allergies2 = new ArrayList<>();
-            allergies2.add("ciprofloxacin");
-            allergies2.add("azithromycin");
-            List<String> medications2 = new ArrayList<>();
-            medications2.add("rosuvastatin");
-            medications2.add("bisoprolol");
-            List<String> medicalConditions2 = new ArrayList<>();
-            medicalConditions2.add("hyperlipidemia");
-            medicalConditions2.add("hypertension");
-            String title3 = "Eye infection";
-            String body3 = "Patient presenting with eye infection";
-            String provider3 = "Dr. Williams";
-            Date visitDate3 = new Date(1, 18, 2025);
-            ClinicalNote note3 = new ClinicalNote(title3, body3, provider3, visitDate3);
-            String title4 = "Skin rash";
-            String body4 = "Patient presenting with skin rash";
-            String provider4 = "Dr. Jones";
-            Date visitDate4 = new Date(2, 12, 2025);
-            ClinicalNote note4 = new ClinicalNote(title4, body4, provider4, visitDate4);
-            List<ClinicalNote> notesList2 = new ArrayList<>();
-            notesList2.add(note3);
-            notesList2.add(note4);
-
-            checkPatientGeneral("Amira", "Garcia", dateOfBirth2, 53, 925645120, patients.get(1));
-            checkPatientAllergies(allergies2, patients.get(1));
-            checkPatientMedications(medications2, patients.get(1));
-            checkPatientMedicalConditions(medicalConditions2, patients.get(1));
-            checkPatientClinicalNotes(notesList2, patients.get(1));
-            checkClinicalNote(title3, body3, provider3, visitDate3, note3);
-            checkClinicalNote(title4, body4, provider4, visitDate4, note4);
-
+            checkClinicInitial(clinic, patients);
+            checkPatientGeneral1(patients);
+            checkPatientAllergies1(patients);
+            checkPatientMedications1(patients);
+            checkPatientMedicalConditions1(patients);
+            checkClinicalNotes1(patients);
+            checkPatientGeneral2(patients);
+            checkPatientAllergies2(patients);
+            checkPatientMedications2(patients);
+            checkPatientMedicalConditions2(patients);
+            checkClinicalNotes2(patients);
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
+    }
+
+    public void checkClinicInitial(Clinic clinic, List<Patient> patients) {
+        assertEquals("My Clinic", clinic.getClinicName());
+        assertEquals(2, patients.size());
+    }
+
+    public void checkPatientGeneral1(List<Patient> patients) {
+        Date dateOfBirth1 = new Date(8, 16, 1965);
+        checkPatientGeneral("Mya", "Cornell", dateOfBirth1, 59, 925245285, patients.get(0));
+    }
+    
+    public void checkPatientAllergies1(List<Patient> patients) {
+        List<String> allergies1 = new ArrayList<>();
+        allergies1.add("penicillin");
+        allergies1.add("codeine");
+        checkPatientAllergies(allergies1, patients.get(0));
+    }
+
+    public void checkPatientMedications1(List<Patient> patients) {
+        List<String> medications1 = new ArrayList<>();
+        medications1.add("metformin");
+        medications1.add("ramipril");
+        medications1.add("amlodipine");
+        checkPatientMedications(medications1, patients.get(0));
+    }
+
+    public void checkPatientMedicalConditions1(List<Patient> patients) {
+        List<String> medicalConditions1 = new ArrayList<>();
+        medicalConditions1.add("diabetes");
+        medicalConditions1.add("hypertension");
+        checkPatientMedicalConditions(medicalConditions1, patients.get(0));
+    }
+
+    public void checkClinicalNotes1(List<Patient> patients) {
+        Date visitDate1 = new Date(1, 16, 2025);
+        ClinicalNote note1 = new ClinicalNote("Coughing", "Patient presenting with severe cough", 
+                "Dr. Campbell", visitDate1);
+        Date visitDate2 = new Date(2, 20, 2025);
+        ClinicalNote note2 = new ClinicalNote("Ear infection", "Patient presenting with infected ear",
+                "Dr. Singh", visitDate2);
+        List<ClinicalNote> notesList1 = new ArrayList<>();
+        notesList1.add(note1);
+        notesList1.add(note2);
+
+        checkPatientClinicalNotes(notesList1, patients.get(0));
+        checkClinicalNote("Coughing", "Patient presenting with severe cough", "Dr. Campbell", visitDate1, note1);
+        checkClinicalNote("Ear infection", "Patient presenting with infected ear", "Dr. Singh", visitDate2, note2);
+    }
+
+    public void checkPatientGeneral2(List<Patient> patients) {
+        Date dateOfBirth2 = new Date(11, 8, 1971);
+        checkPatientGeneral("Amira", "Garcia", dateOfBirth2, 53, 925645120, patients.get(1));
+    }
+
+    public void checkPatientAllergies2(List<Patient> patients) {
+        List<String> allergies2 = new ArrayList<>();
+        allergies2.add("ciprofloxacin");
+        allergies2.add("azithromycin");
+        checkPatientAllergies(allergies2, patients.get(1));
+    }
+
+    public void checkPatientMedications2(List<Patient> patients) {
+        List<String> medications2 = new ArrayList<>();
+        medications2.add("rosuvastatin");
+        medications2.add("bisoprolol");
+        checkPatientMedications(medications2, patients.get(1));
+    }
+
+    public void checkPatientMedicalConditions2(List<Patient> patients) {
+        List<String> medicalConditions2 = new ArrayList<>();
+        medicalConditions2.add("hyperlipidemia");
+        medicalConditions2.add("hypertension");
+        checkPatientMedicalConditions(medicalConditions2, patients.get(1));
+    }
+
+    public void checkClinicalNotes2(List<Patient> patients) {
+        Date visitDate3 = new Date(1, 18, 2025);
+        ClinicalNote note3 = new ClinicalNote("Eye infection", "Patient has eye infection", 
+                "Dr. Williams", visitDate3);
+        Date visitDate4 = new Date(2, 12, 2025);
+        ClinicalNote note4 = new ClinicalNote("Skin rash", "Patient presenting with skin rash",
+                "Dr. Jones", visitDate4);
+        List<ClinicalNote> notesList2 = new ArrayList<>();
+        notesList2.add(note3);
+        notesList2.add(note4);
+
+        checkPatientClinicalNotes(notesList2, patients.get(1));
+        checkClinicalNote("Eye infection", "Patient has eye infection", "Dr. Williams", visitDate3, note3);
+        checkClinicalNote("Skin rash", "Patient presenting with skin rash", "Dr. Jones", visitDate4, note4);
     }
     
 }
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -48,104 +48,183 @@ public class JsonWriterTest extends JsonTest {
     void testWriterGeneralClinic() {
         try {
             Clinic clinic = new Clinic("My Clinic");
-
-            Date dateOfBirth1 = new Date(9, 11, 1968);
-            Patient patient1 = new Patient("Ashley", "Davis", dateOfBirth1, 56, 920215344);
-            patient1.addAllergy("amoxicillin");
-            patient1.addAllergy("ibuprofen");
-            patient1.addMedication("ramipril");
-            patient1.addMedication("amlodipine");
-            patient1.addMedication("escitalopram");
-            patient1.addMedicalCondition("hypertension");
-            patient1.addMedicalCondition("anxiety");
-            String title1 = "Constipation";
-            String body1 = "Patient presenting with severe constipation";
-            String provider1 = "Dr. Johnson";
-            Date visitDate1 = new Date(1, 10, 2025);
-            ClinicalNote note1 = new ClinicalNote(title1, body1, provider1, visitDate1);
-            String title2 = "Urinary tract infection";
-            String body2 = "Patient presenting with signs of a urinary tract infection";
-            String provider2 = "Dr. Kaur";
-            Date visitDate2 = new Date(2, 15, 2025);
-            ClinicalNote note2 = new ClinicalNote(title2, body2, provider2, visitDate2);
-            patient1.addClinicalNote(note1);
-            patient1.addClinicalNote(note2);
+            Patient patient1 = makePatient1();
             clinic.addPatient(patient1);
-
-            Date dateOfBirth2 = new Date(1, 6, 1971);
-            Patient patient2 = new Patient("Thomas", "Smith", dateOfBirth2, 54, 905845652);
-            patient2.addAllergy("tetracycline");
-            patient2.addAllergy("aspirin");
-            patient2.addMedication("metformin");
-            patient2.addMedication("valproic acid");
-            patient2.addMedicalCondition("diabetes");
-            patient2.addMedicalCondition("epilepsy");
-            String title3 = "Allergic rhinitis";
-            String body3 = "Patient presenting with sneezing and nasal congestion";
-            String provider3 = "Dr. Rodriguez";
-            Date visitDate3 = new Date(1, 18, 2025);
-            ClinicalNote note3 = new ClinicalNote(title3, body3, provider3, visitDate3);
-            String title4 = "Cold sore";
-            String body4 = "Patient presenting with new cold sore";
-            String provider4 = "Dr. Young";
-            Date visitDate4 = new Date(2, 12, 2025);
-            ClinicalNote note4 = new ClinicalNote(title4, body4, provider4, visitDate4);
-            patient2.addClinicalNote(note3);
-            patient2.addClinicalNote(note4);
+            Patient patient2 = makePatient2();
             clinic.addPatient(patient2);
 
             JsonWriter writer = new JsonWriter("./data/testWriterGeneralClinic.json");
             writer.open();
             writer.write(clinic);
             writer.close();
-
             JsonReader reader = new JsonReader("./data/testWriterGeneralClinic.json");
             clinic = reader.read();
-            assertEquals("My Clinic", clinic.getClinicName());
+
             List<Patient> patients = clinic.getPatients();
-            assertEquals(2, patients.size());
-
-            List<String> allergies1 = new ArrayList<>();
-            allergies1.add("amoxicillin");
-            allergies1.add("ibuprofen");
-            List<String> medications1 = new ArrayList<>();
-            medications1.add("ramipril");
-            medications1.add("amlodipine");
-            medications1.add("escitalopram");
-            List<String> medicalConditions1 = new ArrayList<>();
-            medicalConditions1.add("hypertension");
-            medicalConditions1.add("anxiety");
-            List<ClinicalNote> notesList1 = new ArrayList<>();
-            notesList1.add(note1);
-            notesList1.add(note2);
-
-            checkPatientGeneral("Ashley", "Davis", dateOfBirth1, 56, 920215344, patients.get(0));
-            checkPatientAllergies(allergies1, patients.get(0));
-            checkPatientMedications(medications1, patients.get(0));
-            checkPatientMedicalConditions(medicalConditions1, patients.get(0));
-            checkPatientClinicalNotes(notesList1, patients.get(0));
-
-            List<String> allergies2 = new ArrayList<>();
-            allergies2.add("tetracycline");
-            allergies2.add("aspirin");
-            List<String> medications2 = new ArrayList<>();
-            medications2.add("metformin");
-            medications2.add("valproic acid");
-            List<String> medicalConditions2 = new ArrayList<>();
-            medicalConditions2.add("diabetes");
-            medicalConditions2.add("epilepsy");
-            List<ClinicalNote> notesList2 = new ArrayList<>();
-            notesList2.add(note3);
-            notesList2.add(note4);
-
-            checkPatientGeneral("Thomas", "Smith", dateOfBirth2, 54, 905845652, patients.get(1));
-            checkPatientAllergies(allergies2, patients.get(1));
-            checkPatientMedications(medications2, patients.get(1));
-            checkPatientMedicalConditions(medicalConditions2, patients.get(1));
-            checkPatientClinicalNotes(notesList2, patients.get(1));
-
+            checkClinicInitial(clinic, patients);
+            checkPatient1(patients);
+            checkPatient2(patients);
         } catch (IOException e) {
             fail("Exception should not have been thrown");
         }
+    }
+
+    public Patient makePatient1() {
+        Date dateOfBirth1 = new Date(9, 11, 1968);
+        Patient patient1 = new Patient("Ashley", "Davis", dateOfBirth1, 56, 920215344);
+        patient1.addAllergy("amoxicillin");
+        patient1.addAllergy("ibuprofen");
+        patient1.addMedication("ramipril");
+        patient1.addMedication("amlodipine");
+        patient1.addMedication("escitalopram");
+        patient1.addMedicalCondition("hypertension");
+        patient1.addMedicalCondition("anxiety");
+        String title1 = "Constipation";
+        String body1 = "Patient presenting with severe constipation";
+        String provider1 = "Dr. Johnson";
+        Date visitDate1 = new Date(1, 10, 2025);
+        ClinicalNote note1 = new ClinicalNote(title1, body1, provider1, visitDate1);
+        String title2 = "Urinary tract infection";
+        String body2 = "Patient presenting with bladder infection";
+        String provider2 = "Dr. Kaur";
+        Date visitDate2 = new Date(2, 15, 2025);
+        ClinicalNote note2 = new ClinicalNote(title2, body2, provider2, visitDate2);
+        patient1.addClinicalNote(note1);
+        patient1.addClinicalNote(note2);
+        return patient1;
+    }
+
+    public Patient makePatient2() {
+        Date dateOfBirth2 = new Date(1, 6, 1971);
+        Patient patient2 = new Patient("Thomas", "Smith", dateOfBirth2, 54, 905845652);
+        patient2.addAllergy("tetracycline");
+        patient2.addAllergy("aspirin");
+        patient2.addMedication("metformin");
+        patient2.addMedication("valproic acid");
+        patient2.addMedicalCondition("diabetes");
+        patient2.addMedicalCondition("epilepsy");
+        String title3 = "Allergic rhinitis";
+        String body3 = "Patient presenting with nasal congestion";
+        String provider3 = "Dr. Rodriguez";
+        Date visitDate3 = new Date(1, 18, 2025);
+        ClinicalNote note3 = new ClinicalNote(title3, body3, provider3, visitDate3);
+        String title4 = "Cold sore";
+        String body4 = "Patient presenting with new cold sore";
+        String provider4 = "Dr. Young";
+        Date visitDate4 = new Date(2, 12, 2025);
+        ClinicalNote note4 = new ClinicalNote(title4, body4, provider4, visitDate4);
+        patient2.addClinicalNote(note3);
+        patient2.addClinicalNote(note4);
+        return patient2;
+    }
+
+    public void checkClinicInitial(Clinic clinic, List<Patient> patients) {
+        assertEquals("My Clinic", clinic.getClinicName());
+        assertEquals(2, patients.size());
+    }
+
+    public void checkPatient1(List<Patient> patients) {
+        checkPatientGeneral1(patients);
+        checkPatientAllergies1(patients);
+        checkPatientMedications1(patients);
+        checkPatientMedicalConditions1(patients);
+        checkClinicalNotes1(patients);
+    }
+
+    public void checkPatientGeneral1(List<Patient> patients) {
+        Date dateOfBirth1 = new Date(9, 11, 1968);
+        checkPatientGeneral("Ashley", "Davis", dateOfBirth1, 56, 920215344, patients.get(0));
+    }
+    
+    public void checkPatientAllergies1(List<Patient> patients) {
+        List<String> allergies1 = new ArrayList<>();
+        allergies1.add("amoxicillin");
+        allergies1.add("ibuprofen");
+        checkPatientAllergies(allergies1, patients.get(0));
+    }
+
+    public void checkPatientMedications1(List<Patient> patients) {
+        List<String> medications1 = new ArrayList<>();
+        medications1.add("ramipril");
+        medications1.add("amlodipine");
+        medications1.add("escitalopram");
+        checkPatientMedications(medications1, patients.get(0));
+    }
+
+    public void checkPatientMedicalConditions1(List<Patient> patients) {
+        List<String> medicalConditions1 = new ArrayList<>();
+        medicalConditions1.add("hypertension");
+        medicalConditions1.add("anxiety");
+        checkPatientMedicalConditions(medicalConditions1, patients.get(0));
+    }
+
+    public void checkClinicalNotes1(List<Patient> patients) {
+        Date visitDate1 = new Date(1, 10, 2025);
+        ClinicalNote note1 = new ClinicalNote("Constipation", "Patient presenting with severe constipation", 
+                "Dr. Johnson", visitDate1);
+        Date visitDate2 = new Date(2, 15, 2025);
+        ClinicalNote note2 = new ClinicalNote("Urinary tract infection", "Patient presenting with bladder infection",
+                "Dr. Kaur", visitDate2);
+        List<ClinicalNote> notesList1 = new ArrayList<>();
+        notesList1.add(note1);
+        notesList1.add(note2);
+
+        checkPatientClinicalNotes(notesList1, patients.get(0));
+        checkClinicalNote("Constipation", "Patient presenting with severe constipation",
+                "Dr. Johnson", visitDate1, note1);
+        checkClinicalNote("Urinary tract infection", "Patient presenting with bladder infection", 
+                "Dr. Kaur", visitDate2, note2);
+    }
+
+    public void checkPatient2(List<Patient> patients) {
+        checkPatientGeneral2(patients);
+        checkPatientAllergies2(patients);
+        checkPatientMedications2(patients);
+        checkPatientMedicalConditions2(patients);
+        checkClinicalNotes2(patients);
+    }
+
+    public void checkPatientGeneral2(List<Patient> patients) {
+        Date dateOfBirth2 = new Date(1, 6, 1971);
+        checkPatientGeneral("Thomas", "Smith", dateOfBirth2, 54, 905845652, patients.get(1));
+    }
+
+    public void checkPatientAllergies2(List<Patient> patients) {
+        List<String> allergies2 = new ArrayList<>();
+        allergies2.add("tetracycline");
+        allergies2.add("aspirin");
+        checkPatientAllergies(allergies2, patients.get(1));
+    }
+
+    public void checkPatientMedications2(List<Patient> patients) {
+        List<String> medications2 = new ArrayList<>();
+        medications2.add("metformin");
+        medications2.add("valproic acid");
+        checkPatientMedications(medications2, patients.get(1));
+    }
+
+    public void checkPatientMedicalConditions2(List<Patient> patients) {
+        List<String> medicalConditions2 = new ArrayList<>();
+        medicalConditions2.add("diabetes");
+        medicalConditions2.add("epilepsy");
+        checkPatientMedicalConditions(medicalConditions2, patients.get(1));
+    }
+
+    public void checkClinicalNotes2(List<Patient> patients) {
+        Date visitDate3 = new Date(1, 18, 2025);
+        ClinicalNote note3 = new ClinicalNote("Allergic rhinitis", "Patient presenting with nasal congestion",
+                "Dr. Rodriguez", visitDate3);
+        Date visitDate4 = new Date(2, 12, 2025);
+        ClinicalNote note4 = new ClinicalNote("Cold sore", "Patient presenting with new cold sore",
+                "Dr. Young", visitDate4);
+        List<ClinicalNote> notesList2 = new ArrayList<>();
+        notesList2.add(note3);
+        notesList2.add(note4);
+
+        checkPatientClinicalNotes(notesList2, patients.get(1));
+        checkClinicalNote("Allergic rhinitis", "Patient presenting with nasal congestion", 
+                "Dr. Rodriguez", visitDate3, note3);
+        checkClinicalNote("Cold sore", "Patient presenting with new cold sore", 
+                "Dr. Young", visitDate4, note4);
     }
 }
